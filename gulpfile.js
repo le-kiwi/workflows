3feaebe1edf77f3faca5a041fcc1352cb1845094
Kieran Bennett <kieranbennett@Kierans-MacBook-Pro.local>
2015-10-24 11:32:12 +1100

    Automated reload for html, sass, js

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,18 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
-
-var sassSources = ['components/sass/style.scss'];	
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html']	
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -27,6 +28,7 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload())
 });
 
 gulp.task('compass', function() {
@@ -38,12 +40,26 @@ gulp.task('compass', function() {
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
+	gulp.watch(htmlSources, ['html']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('deafult', ['coffee','js','compass', 'watch']);
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development/',
+		livereload: true
+	});
+});
+
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('default', ['coffee','js','compass','connect', 'watch']);
